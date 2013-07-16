63a88b11be6c87105fa0ed4dc508465876d97145
Xarlox <carfran1925@hotmail.com>
2013-07-16 16:59:31 -0500

    Ventanas
    Un brebe programa .... sobre manejo de ventanas

---
+++ ventanas/Ventanas.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ventanas;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Ventanas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+//new Ventana1().setVisible(true);
+new Ventana1().show();
+    }
+}
